fb3d1bdb514f5fd9c5cc1a4b6e5877c98bd97130
bryon burke <bryonburke@bryons-Mac-mini.local>
2019-11-24 15:53:58 -0800

    restart project. build on weather app as a template

--- src/js/apiFetch.js
+++ src/js/apiFetch.js
@@ -6,14 +6,13 @@ export class ApiFetch {
 
 
   getDoctor() {
-    const ailment = "cancer";
-    this.calls++;
     return fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=cancer&location=45.523064%2C%20-122.676483%2C100&user_location=45.523064%2C%20-122.676483&skip=0&limit=10&user_key=${process.env.API_KEY}`)
     .then((res) => {
       return res.json();
     })
     .then((json) => {
-      return json.data[0].practices[0].name;
+      return json.data[0].profile.first_name;
+      return json.data[0].profile.last_name;
     });
   }
 }
@@ -23,3 +22,6 @@ export class ApiFetch {
 //   return fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${process.env.API_KEY}&s=${searchTerm}`)
 //     .then((response)=> {
 // 65e78e35f3547881dfd6ad5415c92537
+const array1 = ['a', 'b', 'c'];
+
+array1.forEach(element => console.log(element));
--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
-// UTILITIES
-import $ from 'jquery';
-import 'bootstrap';
-import './css/bootstrap.min.css';
-import './css/styles.css';
+$(document).ready(function() {
+  $('#userAilment').click(function() {
+    const ailment = $('#ailment').val();
+    let request = new XMLHttpRequest();
+    const url =`https://api.betterdoctor.com/2016-03-01/doctors?query=${ailment}&location=45.523064%2C%20-122.676483%2C100&user_location=45.523064%2C%20-122.676483&skip=0&limit=10&user_key=${process.env.API_KEY}`
 
-// MAIN LOGIC
-import { ApiFetch } from './js/apiFetch.js';
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
 
 
-// USER INTERFACE
-$(document).ready(function(){
-  $('#getDoctor').click(() => {
-    apiFetch.getDoctor().then((docter) => {
-      $('.doctorOutput').text(docter);
-
-    });
+    const getElements = function(response) {
+      $('.firstName').text(`First Name ${response.data[0].profile.first_name}`);
+      $('.lastName').text(`Last Name ${response.data[0].profile.last_name}`);
+      $('.street').text(`Address line 1 ${response.data[0].practices[0].visit_address.street}`);
+      $('.street2').text(`Address line 2 ${response.data[0].practices[0].visit_address.street2}`);
+      $('.webLink').text(`Website ${response.data[0].practices[0].website}`);
+      $('.acceptingPatients').text(`Accepting patients? ${response.data[0].practices[0].accepts_new_patients}`);
+    }
   });
-
-  const apiFetch = new ApiFetch();
-
-
 });
